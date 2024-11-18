d9b685c654838e460d43a53346c08e2451819ddc
Aswin Damodaran <aswindamodaran183@gmail.com>
2024-11-18 21:37:56 +0530

    email fix

--- src/App.js
+++ src/App.js
@@ -4,38 +4,54 @@ import { useState } from "react";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const [username,setUsername]=useState("")
-  const [mobileNo,setMobileNo]=useState("")
-  const [email,setEmail]=useState("")
+  const [username, setUsername] = useState("");
+  const [mobileNo, setMobileNo] = useState("");
+  const [email, setEmail] = useState("");
 
-
-  const handleSubmit=()=>{
-    if(email){
-      const check=email.split("")
-      if(!check.includes("@")){
-        alert("Invalid email")
-        return
+  const handleSubmit = () => {
+    e.preventDefault();
+    if (email) {
+      const check = email.split("");
+      if (!check.includes("@")) {
+        alert("Invalid email");
+        return;
       }
     }
-    handleModal()
-  }
+    handleModal();
+  };
 
-  const handleModal=()=>{
-    setIsOpen(prev=> !prev)
-  }
+  const handleModal = () => {
+    setIsOpen((prev) => !prev);
+  };
 
   return (
     <div className="App">
       <h1>User Details Modal</h1>
-      <Button onClick={handleModal} variant="contained">Open Form</Button>
+      <Button onClick={handleModal} variant="contained">
+        Open Form
+      </Button>
       <Modal open={isOpen} onClose={handleModal} className="modal">
         <div className="modal-content">
-          <form onSubmit={handleSubmit} className="forms" >
-            <label for="username" >Username:</label>
-            <input id="username" type="text" value={username} required onChange={(e)=> setUsername(e.target.value)} />
-            <label for="email" >Email Address:</label>
-            <input id="email" type="text" value={email} required onChange={(e)=> setEmail(e.target.value)}/>
-            <Button variant="contained" type="submit" >Submit</Button>
+          <form onSubmit={handleSubmit} className="forms">
+            <label for="username">Username:</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label for="email">Email Address:</label>
+            <input
+              id="email"
+              type="text"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
           </form>
         </div>
       </Modal>
