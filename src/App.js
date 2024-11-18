3935b5f3b238320b2e4caff5b0230e64eb5b1474
Aswin Damodaran <aswindamodaran183@gmail.com>
2024-11-18 22:24:15 +0530

    close functionality

--- src/App.js
+++ src/App.js
@@ -44,54 +44,52 @@ function App() {
       <Button onClick={handleModal} variant="contained">
         Open Form
       </Button>
-      <Modal
-        open={isOpen}
-        onClose={handleModal}
-        className="modal"
-        keepMounted={false}
-        BackdropProps={{
-          style: { pointerEvents: "none" },
-        }}
-      >
-        <div className="modal-content">
-          <form onSubmit={handleSubmit} className="forms">
-            <label htmlFor="username">Username:</label>
-            <input
-              id="username"
-              type="text"
-              value={username}
-              required
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <label htmlFor="email">Email Address:</label>
-            <input
-              id="email"
-              type="text"
-              value={email}
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="phone">Phone Number:</label>
-            <input
-              id="phone"
-              type="text"
-              value={mobileNo}
-              required
-              onChange={(e) => setMobileNo(e.target.value)}
-            />
-            <label htmlFor="dob">Date of Birth:</label>
-            <input
-              id="dob"
-              type="date"
-              value={dob}
-              onChange={(e) => setDob(e.target.value)}
-            />
-            <Button className="submit-button" variant="contained" type="submit">
-              Submit
-            </Button>
-          </form>
+      {isOpen && (
+        <div className="modal" >
+          <div className="modal-content">
+            <form onSubmit={handleSubmit} className="forms">
+              <label htmlFor="username">Username:</label>
+              <input
+                id="username"
+                type="text"
+                value={username}
+                required
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label htmlFor="email">Email Address:</label>
+              <input
+                id="email"
+                type="text"
+                value={email}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="phone">Phone Number:</label>
+              <input
+                id="phone"
+                type="text"
+                value={mobileNo}
+                required
+                onChange={(e) => setMobileNo(e.target.value)}
+              />
+              <label htmlFor="dob">Date of Birth:</label>
+              <input
+                id="dob"
+                type="date"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+              />
+              <Button
+                className="submit-button"
+                variant="contained"
+                type="submit"
+              >
+                Submit
+              </Button>
+            </form>
+          </div>
         </div>
-      </Modal>
+      )}
     </div>
   );
 }
