5802bcbc46f1c75e9ad9bfd572e6e02c252cb037
Aswin Damodaran <aswindamodaran183@gmail.com>
2024-11-18 21:31:54 +0530

    second

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [username,setUsername]=useState("")
   const [mobileNo,setMobileNo]=useState("")
+  const [email,setEmail]=useState("")
 
 
+  const handleSubmit=()=>{
+    if(email){
+      const check=email.split("")
+      if(!check.includes("@")){
+        alert("Invalid email")
+        return
+      }
+    }
+    handleModal()
+  }
 
   const handleModal=()=>{
     setIsOpen(prev=> !prev)
@@ -17,11 +28,14 @@ function App() {
     <div className="App">
       <h1>User Details Modal</h1>
       <Button onClick={handleModal} variant="contained">Open Form</Button>
-      <Modal open={isOpen} className="modal">
+      <Modal open={isOpen} onClose={handleModal} className="modal">
         <div className="modal-content">
-          <form className="forms" >
+          <form onSubmit={handleSubmit} className="forms" >
             <label for="username" >Username:</label>
-            <input id="username" type="text" value={username} />
+            <input id="username" type="text" value={username} required onChange={(e)=> setUsername(e.target.value)} />
+            <label for="email" >Email Address:</label>
+            <input id="email" type="text" value={email} required onChange={(e)=> setEmail(e.target.value)}/>
+            <Button variant="contained" type="submit" >Submit</Button>
           </form>
         </div>
       </Modal>
